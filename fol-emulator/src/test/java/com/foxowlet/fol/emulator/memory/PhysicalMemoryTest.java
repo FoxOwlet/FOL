94da5f3ffdab394eb506233aafd8765030a65476
foxowlet <foxowlet.it@gmail.com>
2023-09-08 13:42:58 +0200

    [fol-emulator] Move PhysicalMemoryTest to its own package

--- fol-emulator/src/test/java/com/foxowlet/fol/emulator/memory/PhysicalMemoryTest.java
+++ fol-emulator/src/test/java/com/foxowlet/fol/emulator/memory/PhysicalMemoryTest.java
@@ -1,6 +1,5 @@
-package com.foxowlet.fol.emulator;
+package com.foxowlet.fol.emulator.memory;
 
-import com.foxowlet.fol.emulator.memory.PhysicalMemory;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
